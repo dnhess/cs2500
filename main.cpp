13e6c4065c23bb925f13f53425e3828e9f5d557d
Dillon Hess <dnhc59@mst.edu>
2017-03-15 14:18:10 -0500

    Generated a graphical representation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 int main() {
+	//Code Idea
+	/*
+	 * The idea of the code is to first generate a grid system that stores 1s
+	 * and 0s. The 1s will represent the actual sensor coordinates and the 0s
+	 * will represent the empty space (Note: The radius of coverage has not
+	 * been shown yet)
+	 *
+	 * To find a sensor the program will loop through the entire grid system
+	 * looking for the 1s. Once it finds a 1 it will check to see if there
+	 * are any 1s in a radius of 5
+	 */
+
 	short sensornumber;
-	//int pointx[50];
-	//int pointy[50];
+	int R = 5;
 	cin >> sensornumber;
+50
 	int grid[50][50];
 	for(int a = 0; a < 50; a++)
 	{
@@ -19,14 +30,14 @@ int main() {
 		int rnd = rand() % (50*50);
 		int x = rnd % 50;
 		int y = rnd / 50;
-		//pointx[x] = 1;
-		//pointy[y] = 1;
 		grid[x][y] = 1;
 	}
 
 	for(int c = 0; c < 50; c++)
 	{
 		for(int d = 0; d < 50; d++)
-			cout <<"x: "<<c<<" y: "<<d<<"Grid Number: "<<grid[c][d]<<endl;
+			cout <<"|"<<grid[c][d]<<"|";
+		cout <<endl;
 	}
+
 }
